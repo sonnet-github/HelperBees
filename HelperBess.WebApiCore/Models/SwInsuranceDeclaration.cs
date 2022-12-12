3280ac293b3e3cc51ff371efc700e71197b60692
Noli Moises JR <nolim@sonnet.digital>
2022-12-12 19:46:11 +0800

    Update Additinal column SwInsuranceDeclaration   Post   Put   Get    Delete SwSupportWorkerLanguage   Update validation

--- HelperBess.WebApiCore/Models/SwInsuranceDeclaration.cs
+++ HelperBess.WebApiCore/Models/SwInsuranceDeclaration.cs
@@ -14,6 +14,18 @@ namespace HelperBess.WebApiCore.Models
         public string BankruptcyDetails { get; set; }
         public bool? Fraud { get; set; }
         public string FraudDetails { get; set; }
+        public bool? Circumstance { get; set; }
+        public string CircumstanceDetails { get; set; }
+        public bool? Insurance { get; set; }
+        public string InsuranceDetails { get; set; }
+        public bool? Workers { get; set; }
+        public string WorkersDetails { get; set; }
+        public bool? MMPI { get; set; }
+        public string MMPIDetails { get; set; }
+        public bool? Liability { get; set; }
+        public string LiabilityDetails { get; set; }
+        public bool? Accident { get; set; }
+        public string AccidentsDetails { get; set; }
 
         public virtual SwSupportWorker SupportWorker { get; set; }
     }
